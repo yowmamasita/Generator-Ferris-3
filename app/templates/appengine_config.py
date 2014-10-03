bd84e189b23e522c1dde9ac9ef153e68cfe2f2e5
Jon Wayne Parrott <jjramone13@gmail.com>
2014-10-02 22:10:52 -0400

    Adding basic handler, updating vendor

--- app/templates/appengine_config.py
+++ app/templates/appengine_config.py
@@ -1,7 +1,5 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 
 # Ensure 3rd-party packages are available.
-
 import vendor
-
-vendor.add_package_dir('lib')
+vendor.add('lib')
